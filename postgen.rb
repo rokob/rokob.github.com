7e4324deeede97d6d51160e5aaf3477219dfe96c
Andrew Ledvina <wvvwwvw@gmail.com>
2016-01-02 17:16:07 -0700

    [ci skip] add some more granularity to the postgen script

--- postgen.rb
+++ postgen.rb
@@ -11,7 +11,7 @@ module PostGen
     def self.run(args)
       if args.length > 0
         if args[0] == 'book_info'
-          gather_book_info
+          gather_book_info('./_posts', args[1])
         elsif args[0] == 'calendar'
           output_calendar
         end
@@ -68,10 +68,15 @@ module PostGen
       "#{type.capitalize}: #{tags.sort.join(', ')}"
     end
 
-    def self.gather_book_info(dir='./_posts')
-      counts, rank_counts, tags = normalize_data(dir)
+    def self.gather_book_info(dir='./_posts', year=nil)
+      counts, rank_counts, tags = normalize_data(dir, year)
       puts "Total count: #{counts.values.reduce(&:+)}"
       puts
+      unless year
+        puts "By year:"
+        output_by_year(counts)
+        puts
+      end
       puts "By month:"
       output_by_month(counts)
       puts
@@ -87,14 +92,14 @@ module PostGen
       puts
     end
 
-    def self.normalize_data(dir='./_posts')
+    def self.normalize_data(dir='./_posts', year=nil)
       tags = Hash.new(0)
       rank_counts = {}
       RANKINGS.each do |rank|
         rank_counts[rank] = 0
       end
       counts = Hash.new(0)
-      walk_posts(dir) do |data|
+      walk_posts(dir, year) do |data|
         if data[:categories].include? 'book'
           data[:tags].each do |tag|
             next if tag == 'book'
@@ -121,7 +126,17 @@ module PostGen
       end
     end
 
-    def self.walk_posts(dir='./_posts')
+    def self.output_by_year(counts)
+      by_year = Hash.new(0)
+      counts.each do |date, count|
+        by_year[date[0..3]] += count
+      end
+      by_year.keys.sort.each do |year|
+        puts "#{year}: #{by_year[year]}"
+      end
+    end
+
+    def self.walk_posts(dir='./_posts', year=nil)
       Dir.foreach(dir) do |name|
         next if name.start_with? '.'
         File.open("#{dir}/#{name}", 'r') do |f|
@@ -144,7 +159,9 @@ module PostGen
                 end
               end
             end
-            yield data if block_given?
+            if !year || data[:date].strftime('%Y') == year
+              yield data if block_given?
+            end
           end
         end
       end
