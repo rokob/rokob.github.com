4a51ced2f93210265f5ee46d92f05dc4a98e44f4
Andrew Weiss <wvvwwvw@gmail.com>
2021-01-22 23:29:20 -0800

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import doggo from "../../content/images/dogmap.jpg"
 export default () => (
   <Layout>
     <p>The venerable personal website about page. Maybe one day I will put content back here, but for now this is it.</p>
+    <p>If you are interested you can find the <a href="https://github.com/rokob/rokob.github.com/tree/source" rel="noopener noreferrer" target="_blank">source that generates this site here</a>.</p>
     <img src={doggo} alt="Doggos" />
   </Layout>
 )
